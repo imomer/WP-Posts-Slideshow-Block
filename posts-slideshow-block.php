53a63e68862ce17aadc7cc0537fb79b847b4bde6
imomer <omerblink@gmail.com>
2022-12-28 08:09:11 +0400

    Completed till javascript slider work.

---
+++ posts-slideshow-block.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Plugin Name: Posts Slideshow Block
+ * Description: Custom gutenberg posts slideshow block for rtCamp assignment.
+ * Author: Omer Hussain
+ * Author URI: https://imomer.com
+ */
+
+/**
+ * Assets
+ */
+function posts_slideshow_block_assets()
+{
+  // CSS
+  wp_enqueue_style('posts-slideshow-block-style', plugin_dir_url(__FILE__) . 'psb-style.css', [], true);
+
+  // JS
+  wp_enqueue_script('posts-slideshow-block-js', plugin_dir_url(__FILE__) . 'psb-script.js', ['wp-blocks', 'wp-element', 'wp-editor'], true);
+}
+add_action('enqueue_block_assets', 'posts_slideshow_block_assets');
+
+/**
+ * Dynamic Block Render
+ */
+function psb_dynamic_block_render_callback($attributes)
+{
+
+  // Vars
+  $remotePosts = $attributes['remotePosts'];
+  $hidePostThumb = $attributes['hidePostThumb'];
+  $hidePostDate = $attributes['hidePostDate'];
+  $hidePostExcerpt = $attributes['hidePostExcerpt'];
+
+  // echo '<pre>', print_r($attributes), '</pre>';
+  // die;
+
+  // Output
+  ob_start();
+  if (isset($remotePosts) && count($remotePosts) > 0) {
+
+    echo '<div id="psb__wrapper">';
+    echo '<div class="psb__slider">';
+
+    // Post loop
+    foreach ($remotePosts as $remotePost) {
+
+      echo '<div class="psb__slide">';
+
+      if (!isset($hidePostThumb) || $hidePostThumb == '') {
+        echo '<div class="psb__post-thumb" style="background-image: url(' . $remotePost['episode_featured_image'] . ')"></div>';
+      }
+
+      echo '<h6 class="psd__post-title"><a href="' . $remotePost['link'] . '" target="_blank">' . $remotePost['title']['rendered'] . '</a></h6>';
+
+      if (!isset($hidePostDate) || $hidePostDate == '') {
+        echo '<div class="psb__post-date">' . date('F j, Y', strtotime($remotePost['date'])) . '</div>';
+      }
+
+      if (!isset($hidePostExcerpt) || $hidePostExcerpt == '') {
+        echo '<div class="psb__post-excerpt">' . $remotePost['excerpt']['rendered'] . '</div>';
+      }
+
+      echo '</div>';
+    }
+
+    echo "</div>";
+    echo "</div>";
+  }
+
+  return ob_get_clean();
+}
+
+/**
+ * Dynamic Block
+ */
+function psb_dynamic_block()
+{
+  register_block_type('psb/posts-slideshow-block', array(
+    'render_callback' => 'psb_dynamic_block_render_callback'
+  ));
+}
+add_action('init', 'psb_dynamic_block');
---
+++ psb-script.js
@@ -0,0 +1,101 @@
+/**
+ * Posts Slideshow Block Script
+ * This is where the magic 🪄 happens
+ */
+((blocks, element, components) => {
+  const el = element.createElement;
+
+  // Register Block Type
+  blocks.registerBlockType("psb/posts-slideshow-block", {
+    title: "Posts Slideshow",
+    description:
+      "Custom gutenberg posts slideshow block for rtCamp assignment.",
+    icon: "embed-photo",
+    category: "widgets",
+    attributes: {
+      remotePosts: { type: "object" },
+      remotePostsSaved: { type: "boolean" },
+      hidePostThumb: { type: "boolean" },
+      hidePostDate: { type: "boolean" },
+      hidePostExcerpt: { type: "boolean" },
+    },
+    keywords: ["slider", "posts", "slideshow"],
+
+    // Edit
+    edit: (props) => {
+      // Vars
+      var remotePosts = props.attributes.remotePosts,
+        remotePostsSaved = props.attributes.remotePostsSaved,
+        hidePostThumb = props.attributes.hidePostThumb,
+        hidePostDate = props.attributes.hidePostDate,
+        hidePostExcerpt = props.attributes.hidePostExcerpt;
+
+      // Get & Set posts
+      if (!remotePosts) {
+        fetch("https://wptavern.com/wp-json/wp/v2/posts")
+          .then((res) => {
+            return res.json();
+          })
+          .then((posts) =>
+            props.setAttributes({ remotePosts: posts, remotePostsSaved: true })
+          );
+      }
+
+      // On change
+      const changeHidePostDate = () => {
+        props.setAttributes({ hidePostDate: !hidePostDate });
+      };
+      const changeHidePostExcerpt = () => {
+        props.setAttributes({ hidePostExcerpt: !hidePostExcerpt });
+      };
+      const changeHidePostThumb = () => {
+        props.setAttributes({ hidePostThumb: !hidePostThumb });
+      };
+
+      return el(
+        "div",
+        { className: "psb-editor-placeholder" },
+        el(
+          "div",
+          { className: "psb-edit__row" },
+          el("h6", { className: "psb-edit__heading" }, "Posts Slideshow Block"),
+          !remotePostsSaved &&
+            el(
+              "div",
+              null,
+              el(
+                "small",
+                { style: { position: "relative", top: -3, color: "#A9A9A9" } },
+                "Fetching Posts"
+              ),
+              el(components.Spinner)
+            )
+        ),
+        el(
+          "div",
+          { className: "psb-edit__row" },
+          el(components.ToggleControl, {
+            label: "Hide Post Thumb",
+            checked: hidePostThumb,
+            onChange: changeHidePostThumb,
+          }),
+          el(components.ToggleControl, {
+            label: "Hide Post Date",
+            checked: hidePostDate,
+            onChange: changeHidePostDate,
+          }),
+          el(components.ToggleControl, {
+            label: "Hide Post Excerpt",
+            checked: hidePostExcerpt,
+            onChange: changeHidePostExcerpt,
+          })
+        )
+      );
+    },
+
+    // Save
+    save: (props) => {
+      return null;
+    },
+  });
+})(window.wp.blocks, window.wp.element, window.wp.components);
